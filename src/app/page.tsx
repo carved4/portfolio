acf1008b1e259907b8f232361c6d98ed438165ad
carved4 <osmith26@adrian.edu>
2024-12-02 16:51:57 -0500

    Fix: Update import paths to use relative paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Hero from '~/components/Hero'
-import About from '~/components/About'
-import Skills from '~/components/Skills'
-import Projects from '~/components/Projects'
-import Contact from '~/components/Contact'
-import Navbar from '~/components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
